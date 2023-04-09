d8b0feb01fc2ce36f2468b827b228f1a1951cba4
Arom Burgos <gonzalesburgosaromsadith2004@gmail.com>
2023-04-08 22:25:59 -0500

    actualizando cambios 1.0

--- js/index.js
+++ js/index.js
@@ -7,11 +7,30 @@ function cambiarModo(){
 
     if(body.classList.contains("modoDark")){
         body.classList.remove("modoDark")
-        botonClaro.innerHTML = '<span class="material-symbols-outlined" id="botonClaro" style="font-weight: bold;"> light_modeModo Claro</span>' 
+        botonClaro.innerHTML = '<span class="material-symbols-outlined"style="font-weight: bold;"> light_modeModo Claro</span>' 
     }
     else{
         body.classList.add("modoDark")
-        botonClaro.innerHTML = '<span class="material-symbols-outlined" id="botonClaro" style="font-weight: bold;"> dark_modeModo Oscuro</span>' 
+        botonClaro.innerHTML = '<span class="material-symbols-outlined"style="font-weight: bold;"> dark_modeModo Oscuro</span>' 
     }
 }
-botonClaro.addEventListener("click", cambiarModo)
+botonClaro.addEventListener("click", cambiarModo);
+
+
+
+/* Musica */
+let audio = document.getElementById("audio");
+let botonAudio = document.getElementById("botonAudio");
+
+function activarAudio(){
+   
+    if (audio.paused){
+        audio.play();
+        botonAudio.innerHTML = '<span class="material-icons-outlined">headset</span>' 
+    }
+    else{
+        audio.pause();
+        botonAudio.innerHTML = '<span class="material-icons-outlined">headset_off</span>' 
+    }
+}
+botonAudio.addEventListener("click", activarAudio)
